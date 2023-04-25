bb22b679a8ff94205ec489b5c7647e1dbc49b699
Anastasia <sychkova.nastyusha@mail.ru>
2023-04-25 19:59:19 +0400

    код не запускается

--- src/main/java/com/example/kurs4_1/Kurs41Application.java
+++ src/main/java/com/example/kurs4_1/Kurs41Application.java
@@ -1,9 +1,11 @@
 package com.example.kurs4_1;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@OpenAPIDefinition
 public class Kurs41Application {
 
     public static void main(String[] args) {
